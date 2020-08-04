aa97f507d498217f422049e77d2cadc91aa23b51
Drew Ross <mail.drew.ross@gmail.com>
2020-08-04 14:58:27 -0700

    Update PUT request handler to return newly updated post.

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -79,7 +79,9 @@ router.put('/:id', (req, res) => {
   db.update(req.params.id, req.body)
     .then(post => {
       if (post) {
-        res.status(200).json({ message: 'The post has been updated.' });
+        db.findById(req.params.id)
+          .then(newPost => res.status(200).json(newPost))
+          .catch(err => res.status(500).json({ error: 'The post may have been updated, but the server could not return the updated post.' }));
       } else {
         res.status(404).json({ message: 'The post with the specified ID does not exist.' });
       }
